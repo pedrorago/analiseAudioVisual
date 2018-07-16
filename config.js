53d0679d882fe324798264dcca2b3f5d77631344
Roberto Oliveira <betonix@MacBook-Air-de-Roberto.local>
2018-07-16 19:27:58 -0300

    static folder

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ var config = {
 	database: {
 		host:	  'localhost', 	// database host
 		user: 	  'root', 		// your database username
-		password: '', 		// your database password
+		password: '123456', 		// your database password
 		port: 	  3306, 		// default MySQL port
-		db: 	  'test' 		// your database name
+		db: 	  'saav' 		// your database name
 	},
 	server: {
 		host: '127.0.0.1',
